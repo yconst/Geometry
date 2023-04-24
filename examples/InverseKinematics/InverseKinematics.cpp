1639c314c313630ba092453010e66b9c02214313
Yannis Chatzikonstantinou <info@tinymovr.com>
2023-04-25 00:13:04 +0300

    impl

--- examples/InverseKinematics/InverseKinematics.cpp
+++ examples/InverseKinematics/InverseKinematics.cpp
@@ -23,20 +23,9 @@ Joint::Joint(Link &parent, Link &child, const Geometry::Twist &axis_, const Geom
     child.parent = &parent;
 }
 
-// Function to compute the cross product of two 3D vectors
-Matrix<3> cross_product(const Matrix<3>& a, const Matrix<3>& b) {
-    Matrix<3> result;
-
-    result(0) = a(1) * b(2) - a(2) * b(1);
-    result(1) = a(2) * b(0) - a(0) * b(2);
-    result(2) = a(0) * b(1) - a(1) * b(0);
-
-    return result;
-}
-
 template <int n>
-float Norm(const Matrix<n>& vec) {
-    T sum = 0;
+float norm(const Matrix<n>& vec) {
+    float sum = 0;
     for (int i = 0; i < n; ++i) {
         sum += vec(i) * vec(i);
     }
@@ -52,36 +41,25 @@ bool inverse_kinematics(Link& end_effector, const Geometry::Pose& target_pose, f
 
         // Compute the error in translation and rotation
         Geometry::Translation position_error = target_pose.p - current_pose.p;
-        Geometry::AngularVelocity orientation_error = (cross_product(current_pose.R.Column(0), target_pose.R.Column(0)) +
-                                                       cross_product(current_pose.R.Column(1), target_pose.R.Column(1)) +
-                                                       cross_product(current_pose.R.Column(2), target_pose.R.Column(2)) ) * 0.5f;
 
         // Check for convergence
-        if (position_error.Norm() < tolerance && orientation_error.Norm() < tolerance)
+        if (norm(position_error) < tolerance < tolerance)
         {
             return true;
         }
 
         // Calculate the Jacobian matrix
         BLA::Matrix<6, 6> J = BLA::Zeros<6, 6>();
-        Link* link = &end_effector;
-        while (link->joint)
-        {
-            Geometry::Twist joint_axis = link->joint->axis;
-            Geometry::Wrench joint_wrench = adjoint(link->pose.inverse()) * joint_axis;
-
-            J = J - joint_wrench * (link->parent->twist * joint_axis).Norm();
-
-            link = link->parent;
-        }
 
         // Compute the change in joint angles
-        BLA::Matrix<6> delta_twist = position_error;
-        delta_twist.Append(orientation_error);
-        BLA::Matrix<6> delta_joint_angles = step_size * J.Transpose() * delta_twist;
+        BLA::Matrix<6> delta_twist = BLA::Zeros<6>();
+        delta_twist(0) = position_error(0);
+        delta_twist(1) = position_error(1);
+        delta_twist(2) = position_error(2);
+        BLA::Matrix<6> delta_joint_angles = (~J) * delta_twist * step_size;
 
         // Update joint angles
-        link = &end_effector;
+        auto link = &end_effector;
         for (int j = 0; j < 6; ++j)
         {
             if (!link->joint) break;
@@ -90,6 +68,5 @@ bool inverse_kinematics(Link& end_effector, const Geometry::Pose& target_pose, f
             link = link->parent;
         }
     }
-
     return false;
 }
